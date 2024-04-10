13715cfc721d4976848f5bb993b6c3779625ea45
DC <i6zi@qq.com>
2024-04-10 16:34:02 +0100

    添加英文注释

--- heatmap_cnn.py
+++ heatmap_cnn.py
@@ -33,6 +33,7 @@ class HeatMapCNN(nn.Module):
 def read_data(f_num,d,degree):
     if degree == 0:
         # 设置图像文件夹和标签文件夹的路径
+        # set Image file and label file path
         image_folder = f'/Users/darren/资料/SPIF_DU/Croppings/f{f_num}_out/{d}mm/images'
         label_folder = f'/Users/darren/资料/SPIF_DU/Croppings/f{f_num}_out/{d}mm/labels'
     else:
@@ -41,29 +42,36 @@ def read_data(f_num,d,degree):
     image_files = [os.path.join(image_folder, file) for file in os.listdir(image_folder) if file.endswith('.jpg')]
 
     # 创建空的训练数据列表，用于存储图像和标签
-    X = []  # 用于存储图像数据
-    y = []  # 用于存储标签
+    # Create empty list to store iamges and lables
+    X = []  # 用于存储图像数据 store images
+    y = []  # 用于存储标签 store lables
 
     # 遍历图像文件列表
+    # Iterate Images
     for image_path in image_files:
         # 获取图像文件名，不包括路径和文件扩展名
+        # Get images file name
         image_filename = os.path.splitext(os.path.basename(image_path))[0]
 
         # 构建相应的标签文件路径
+        # Build path for label file
         label_path = os.path.join(label_folder, f'{image_filename}.txt')
 
         # 读取标签文件内容
+        # Read label file
         with open(label_path, 'r') as label_file:
             label = label_file.read().strip()  # 假设标签是一行文本
 
         # 打开图像文件并进行预处理
+        # open image file and do preprocessing
         with Image.open(image_path) as img:
             # 这里可以添加图像预处理步骤，例如将图像调整为固定大小、归一化等
             # img = img.convert("L")
-            img = np.array(img)  # 将图像转化为NumPy数组
+            img = np.array(img)  # 将图像转化为NumPy数组 transfer image to Numpy array
         img = img.transpose((2, 0, 1))
         # img = img.reshape(1,15,15)
         # 将图像数据和标签添加到列表
+        # put image and label into list
         X.append(img)
         y.append(label)
     return X,y
@@ -72,8 +80,8 @@ def read_data(f_num,d,degree):
 # f_num = 3
 # d = 5
 # X,y = read_data(3,5)
-X = []  # 用于存储图像数据
-y = []  # 用于存储标签
+X = []  # 用于存储图像数据 store images
+y = []  # 用于存储标签 store labels
 
 for fum in fums:
     for grid in grids:
@@ -83,15 +91,18 @@ for fum in fums:
             y+=y_fum_grid_degree
 
 # 将X和y转化为NumPy数组
+# transfer X and y into numpy array
 X = np.array(X)
 y = np.array(y)
 
 
 # 归一化图像数据
-X = X / 255.0  # 假设使用0-255的像素值
+# Normalise
+X = X / 255.0  # 使用0-255的像素值
 batch = 8
 
 # 划分数据集为训练集和验证集
+# Divide dataset into train set and validation set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
 
 X_train = X_train.astype('float32')
@@ -129,6 +140,7 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(X_train)):
     # y_train, y_test = y_train_tensor[train_idx], y_train_tensor[test_idx]
 
     # 将剩下的9份按照8:2的比例划分为训练集和验证集
+    # Divide the remaining 9 parts into training sets and validation sets in a ratio of 8:2
     train_size = int(0.8 * len(train_idx))
     X_train_fold, X_val_fold = X_train[:train_size], X_train[train_size:]
     y_train_fold, y_val_fold = y_train[:train_size], y_train[train_size:]
@@ -172,16 +184,20 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(X_train)):
     # 在这里你可以使用 X_train, y_train 进行模型的训练
     # 使用 X_val, y_val 进行模型的验证
     # 创建模型实例
+    # Create model
     model = HeatMapCNN()
     # print(model)
     # 定义损失函数和优化器
+    # Defein optimizer and criterion
     criterion = nn.MSELoss()
     optimizer = optim.Adam(model.parameters(), lr=0.000002) #lr
     # 记录训练和测试过程中的损失
-    train_losses = []  # 训练损失
-    test_losses = []  # 测试损失
+    # store loss during train and test
+    train_losses = []  # 训练损失 train loss
+    test_losses = []  # 测试损失 test loss
 
     # 训练模型
+    # train model
     num_epochs = 1000
     for epoch in range(num_epochs):
         for images, labels in train_data_loader_fold:
@@ -192,6 +208,7 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(X_train)):
             loss.backward()
             optimizer.step()
             # 查看梯度情况
+            # Checkt Gradient
             # for name, parms in model.named_parameters(): 
             #     print('-->name:', name, '-->grad_requirs:',parms.requires_grad, ' -->grad_value:',parms.grad)
         train_losses.append(loss.item() )
@@ -203,6 +220,7 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(X_train)):
 
     # 使用 X_test, y_test 进行最终的测试评估
     # 训练完成后，你可以使用模型进行预测等任务
+    # After training is completed,  use the model for prediction
     model.eval()
     pre = model(X_val_tensor_fold)
     if torch.cuda.is_available():
@@ -216,6 +234,7 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(X_train)):
     r2=r2_score(y_val_tensor_fold,pre)
 
     #记录的是 测试集的结果
+    # Store test data result
     # result_file_path = f'/Users/darren/资料/SPIF_DU/Croppings/result/{grids[0]}mm/tencrosvalidationresult_validate.txt'
     # with open(result_file_path,"a") as file:
     #         file.write(str(fold+1))
@@ -233,7 +252,8 @@ for fold, (train_idx, test_idx) in enumerate(kf.split(X_train)):
     # # 打印当前折数和各集合的大小
     # print(f"Fold {fold + 1} - Train: {len(X_train)}, Validation: {len(X_val)}, Test: {len(X_test)}")
    
-#validation的结果
+# validation的结果
+# Result of validation
 model.eval()
 pre = model(X_test_tensor)
 if torch.cuda.is_available():
